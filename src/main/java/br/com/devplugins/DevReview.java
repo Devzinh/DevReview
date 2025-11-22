d2bf0193e02c054865d2ab70dd8e8807db0711f3
Devzinh <lookplays7@gmail.com>
2025-11-21 23:47:39 -0300

    feat: initialize new Gradle-based Spigot plugin project structure.

---
+++ src/main/java/br/com/devplugins/DevReview.java
@@ -0,0 +1,17 @@
+package br.com.devplugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevReview extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
